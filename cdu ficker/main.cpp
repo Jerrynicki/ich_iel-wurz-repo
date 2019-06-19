1c0433475e184c8657bcb6eba652834aa755e031
Niklas <niklas.schmidt44@gmail.com>
2019-06-19 17:12:09 +0200

    cdu ficker hintzfeweffüfgt

---
+++ cdu ficker/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <thread>
+#include <limits.h>
+
+void fuckthecdu_yeahdigga()
+{
+	while (true)
+	{
+		std::cout << "cdu stinkert hart ";
+	}
+}
+
+int main()
+{
+	std::thread t1(fuckthecdu_yeahdigga);
+	std::thread t2(fuckthecdu_yeahdigga);
+	std::thread t3(fuckthecdu_yeahdigga);
+	std::thread t4(fuckthecdu_yeahdigga);
+	std::thread t5(fuckthecdu_yeahdigga);
+	std::thread t6(fuckthecdu_yeahdigga);
+	std::thread t7(fuckthecdu_yeahdigga);
+	std::thread t8(fuckthecdu_yeahdigga);
+	std::thread t9(fuckthecdu_yeahdigga);
+	std::thread t10(fuckthecdu_yeahdigga);
+	std::thread t11(fuckthecdu_yeahdigga);
+	std::thread t12(fuckthecdu_yeahdigga);
+	std::thread t13(fuckthecdu_yeahdigga);
+	std::thread t14(fuckthecdu_yeahdigga);
+	std::thread t15(fuckthecdu_yeahdigga);
+	std::thread t16(fuckthecdu_yeahdigga);
+	std::thread t17(fuckthecdu_yeahdigga);
+	std::thread t18(fuckthecdu_yeahdigga);
+	std::thread t19(fuckthecdu_yeahdigga);
+	std::thread t20(fuckthecdu_yeahdigga);
+
+	t1.join();
+
+	return 0;
+}
